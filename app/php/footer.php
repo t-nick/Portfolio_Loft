afa5ff64c4d31e1950f1771d0e2fd4dba15f0258
Tanya <nickolaychuk.t@gmail.com>
2017-03-23 12:40:20 +0200

    begin 3rd page

---
+++ app/php/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="border-footer"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+            <div class="footer_block"> 
+            &#169; 2017 Это мой сайт, пожалуйста, не копируйте его.
+            </div>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ app/php/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Portfolio</title>
+	<!-- SEO -->
+	<meta name="description" content="portfolio tanya slipachenko">
+	<meta name="keywords" content="portfolio work job project freelance website site landing page">
+	<meta name="author" content="Slipachenko Tanya">
+	<!-- font-awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Alegreya|Alegreya+Sans:700|Playfair+Display:700|Raleway:200" rel="stylesheet">
+  <link href="fonts/Open Sans/fonts.css">
+  <link rel="stylesheet" type="text/css" href="fonts/fira/fira.css">
+  <link rel="stylesheet" type="text/css" href="fonts/Proxima-Nova/fonts.css">
+  
+  <link rel="stylesheet" type="text/css" href="css/fonts.css">  
+	<!-- libs CSS -->
+	<link rel="stylesheet" href="css/libs.min.css">
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/media.css">
+  <!-- <link rel="stylesheet" href="css/style.min.css"> -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <header class="header">
+    <div class="container">
+      <div class="row">
+          <div class="col-md-3 logo">
+            <img src="img/logo.png" alt="logo">
+          </div>
+
+          <div class="col-md-9">
+            <ul class="nav navbar-nav navbar-right soc">
+              <li><a href="https://www.linkedin.com/in/tanyaslipachenko"><i class="fa fa-linkedin"></i></a></li>
+              <li><a href="https://github.com/t-nick"><i class="fa fa-github-alt"></i></a></li>
+              <li><a href="https://www.facebook.com/profile.php?id=100007857307130"><i class="fa fa-facebook"></i></a></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+    <div class="border"></div>
+  </header>
